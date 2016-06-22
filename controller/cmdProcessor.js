09b94fd9e4ea0fe5049b28fefa2f58d58e114fc8
sihyun <gthpgth@gmail.com>
2016-06-23 02:15:55 +0900

    봐 명령 복구

--- controller/cmdProcessor.js
+++ controller/cmdProcessor.js
@@ -3,39 +3,35 @@ var striptags = require('striptags');
 var g_roomManager;
 var g_combat;
 
-var CmdProcessor = function()
-{
-}
+var CmdProcessor = function() {}
 
-CmdProcessor.prototype.parser = function(socket, data)
-{ 
-        var msg = striptags(data.message);
-        var split = msg.split(' ');
-        var room = g_roomManager.GetById(socket.obj.roomId);
+CmdProcessor.prototype.parser = function(socket, data) {
+    var msg = striptags(data.message);
+    var split = msg.split(' ');
+    var room = g_roomManager.GetById(socket.obj.roomId);
 
-        //        io.sockets.in('room' + this.roomId).emit('send:message', msg);
+    //        io.sockets.in('room' + this.roomId).emit('send:message', msg);
 
-        if (split.length == 1) {
-            switch (split[0]) {
-                case '봐':
-                    sendMsg(socket, makeTexts.MakeRoomPacket(socket));
-                    return;
+    if (split.length == 1) {
+        switch (split[0]) {
+            case '봐':
+                socket.sendMsg(makeTexts.MakeRoomPacket(room, socket));
+                return;
 
-            }
-        } else {
-            var obj = room.GetObjByName(split[0]);
-            if (obj) {
-                if (split.length >= 2 && split[1] == "쳐") {
-                    g_combat.Combat(socket.obj, obj);
-                    return;
-                }
+        }
+    } else {
+        var obj = room.GetObjByName(split[0]);
+        if (obj) {
+            if (split.length >= 2 && split[1] == "쳐") {
+                g_combat.Combat(socket.obj, obj);
+                return;
             }
         }
-        room.SendChat(socket.obj, msg);
+    }
+    room.SendChat(socket.obj, msg);
 }
 
-module.exports = function(roomManager, combat)
-{ 
+module.exports = function(roomManager, combat) {
     g_roomManager = roomManager;
     g_combat = combat;
     return new CmdProcessor();
