7a76931a4e2f4e762016950b11e72d0df20092dd
painh <gthpgth@gmail.com>
2017-06-22 03:11:39 +0900

    지도 추가하면서 좌표계 추가

--- json/zones/zone_0.js
+++ json/zones/zone_0.js
@@ -1,10 +1,12 @@
 module.exports = {
     "room_entry": {
+        "x" : 0,
+        "y" : 0,
         "protoId": "room_entry",
         "displayName": "해안",
         "description": "모래 사장이 펼쳐진 바닷가다. 부드러운 모래 벌판이다.",
         "proto_objects": {"1": {"protoId": 1, "respawnSec": 1}},
-        "exits": {"동": "entry_2"},
+        "exits": {},
         "items": [{
             'id': 'rock',
             "genDeplySec": 0,
@@ -12,10 +14,30 @@ module.exports = {
         }]
     },
     "entry_2": {
+        "x" : 1,
+        "y" : 0,
         "protoId": "entry_2",
         "displayName": "인트로2",
         "description": "인트로 설명",
         "proto_objects": {"1": {"protoId": 1, "respawnSec": 1}},
-        "exits": {"서": "room_entry"}
+        "exits": {}
+    },
+    "entry_3": {
+        "x" : -1,
+        "y" : 0,
+        "protoId": "entry_3",
+        "displayName": "인트로3",
+        "description": "인트로 설명",
+        "proto_objects": {"1": {"protoId": 1, "respawnSec": 1}},
+        "exits": {}
+    },
+    "entry_4": {
+        "x" : 0,
+        "y" : 1,
+        "protoId": "entry_4",
+        "displayName": "인트로4",
+        "description": "인트로 설명",
+        "proto_objects": {"1": {"protoId": 1, "respawnSec": 1}},
+        "exits": {}
     }
 };
